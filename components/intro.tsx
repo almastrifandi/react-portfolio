a946e0013a5be2bb351c4d1c788503fb931e129e
Al <112550077+almastrifandi@users.noreply.github.com>
2024-09-25 00:51:30 +0700

    adding linkto

--- components/intro.tsx
+++ components/intro.tsx
@@ -108,7 +108,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/almas-trifandi-putra-24a5462a7/"
           target="_blank"
         >
           <BsLinkedin />
@@ -116,7 +116,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/almastrifandi"
           target="_blank"
         >
           <FaGithubSquare />
