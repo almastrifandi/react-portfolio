0e085102108fba355001e26c27b9b9766c2573af
Al <112550077+almastrifandi@users.noreply.github.com>
2024-09-25 00:36:57 +0700

    final

--- components/about.tsx
+++ components/about.tsx
@@ -11,7 +11,7 @@ export default function About() {
   return (
     <motion.section
       ref={ref}
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      className="mb-28 max-w-[55rem] text-center leading-8 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
@@ -19,30 +19,31 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        Saat ini saya masih bersekolah di{" "}
+        <span className="font-medium">SMK Telkom 1 Medan</span> dengan jurusan{" "}
+        <span className="font-medium">Rekayasa Perangkat Lunak</span>. Saya
+        sangat menikmati dunia programming, terutama kebahagiaan melihat orang
+        lain menggunakan hasil kerja saya. Saat ini, saya fokus mempelajari{" "}
+        <span className="font-medium">Laravel dan React</span> untuk memperdalam
+        keterampilan saya dalam pengembangan web.
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        Saya selalu mengikuti perkembangan teknologi, terutama dengan kemajuan{" "}
+        <span className="font-medium">AI</span> di zaman sekarang. Saya berani
+        bilang kalau saya adalah seorang{" "}
+        <span className="font-medium">tech enthusiast</span> yang selalu
+        tertarik untuk belajar hal-hal baru di dunia teknologi.
+      </p>
+
+      <p>
+        <span className="italic">Di luar coding</span>, saya hobi bermain{" "}
+        <span className="font-medium">game</span>, membaca{" "}
+        <span className="font-medium">manga</span>, menonton{" "}
+        <span className="font-medium">series</span>,{" "}
+        <span className="font-medium">film</span>, dan{" "}
+        <span className="font-medium">anime</span>. Saya juga sudah menjajal
+        banyak game yang menambah pengalaman dan keseruan di waktu luang.
       </p>
     </motion.section>
   );
